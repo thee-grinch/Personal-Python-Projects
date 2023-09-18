f7ca9ac6bb8d85073d26d4c52cb67e9c38155768
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-09-18 23:59:27 +0300

    commit_message

---
+++ loops/compound_interest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#this is a simple function that calculates compound interest after 10 years
+principle, interest = input("Please enter principle and interest rate: ").split()
+principle = float(principle)
+interest = float(interest)
+interest = interest / 100
+for i in range(10):
+    principle = principle + (principle * interest)
+#printing the outcome
+print("The total amount accumulatted is {:.2f}".format(principle))
