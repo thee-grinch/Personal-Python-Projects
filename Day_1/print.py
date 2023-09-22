c93809d1442a799d16df4b6ca0aef89dea8b146b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-09-22 23:47:22 +0300

    commit_message

---
+++ Day_1/print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" this code receives a two digit number as input and finds their sum"""
+nums = input("Type a two digit number: ")
+num1 = int(nums[0])
+num2 = int(nums[1])
+print(num1 + num2)
